dc711dc069bbdc8cff20d0b8da43bed6be41e2a2
Carlos Fins <clockwerkz@gmail.com>
2023-12-01 13:57:12 -0800

    working on Day 1 problem 2 solution

--- day_01/one.js
+++ day_01/one.js
@@ -1,3 +1,16 @@
 module.exports = function(input) {
-    return 0;
+    let sum = 0;
+    const calibrationValues = input.split('\n');
+    for (calibrationValue of calibrationValues) {
+        const digitsArray = calibrationValue.match(/\d/g);
+        if (!digitsArray) continue;
+        const [ firstDigit ] = digitsArray;
+        if (digitsArray.length < 2) {
+            sum += parseInt(`${firstDigit}${firstDigit}`);
+        } else {
+            const lastDigit = digitsArray[digitsArray.length - 1];
+            sum += parseInt(`${firstDigit}${lastDigit}`);
+        }
+    }
+    return sum;
 }
--- day_01/two.js
+++ day_01/two.js
@@ -1,3 +1,41 @@
+const numberConversion = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
+
+function convertNumber(el) {
+    return isNaN(el) ? numberConversion[el] : el;
+}
+
 module.exports = function(input) {
-    return 0;
+    let sum = 0;
+    const calibrationValues = input.split('\n');
+    const regexStr = Object.keys(numberConversion).join('|'); 
+    const regex = new RegExp(`[0-9]|${regexStr}`, "g");
+    console.log(regex);
+    for (let i = 0; i < calibrationValues.length; i++) {
+        const calibrationValue = calibrationValues[i];
+        const digitsArray = calibrationValue.match(regex);
+        if (!digitsArray) continue;
+        let number;
+        const [ firstDigit ] = digitsArray;
+        if (digitsArray.length < 2) {
+        
+            number = parseInt(`${convertNumber(firstDigit)}${convertNumber(firstDigit)}`);
+        } else {
+            const lastDigit = digitsArray[digitsArray.length - 1];
+            number = parseInt(`${convertNumber(firstDigit)}${convertNumber(lastDigit)}`);
+        }
+        console.log(`${calibrationValue}: ${number} + ${sum} = ${number + sum}`);
+        sum += number;
+        // console.log(calibrationValue, digitsArray, number);
+    }
+    return sum;
 }
